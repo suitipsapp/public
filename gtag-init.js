aa7e69db4c4205c4c5835f0da799e0c4daaa4007
suitipsapp <suitips.app@gmail.com>
2025-11-23 10:24:02 -0500

    Create gtag-init.js

---
+++ gtag-init.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  window.dataLayer.push(arguments);
+}
+
+gtag('js', new Date());
+gtag('config', 'G-PRZ0HXPG81', { send_page_view: false });
